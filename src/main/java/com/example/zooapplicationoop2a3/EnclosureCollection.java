a934e8c1192ea1f1766f0b4b31a4f3298851d5c2
OlivierBachand <olivier.bachand@stu.crc-lennox.qc.ca>
2025-10-30 18:34:39 -0400

    Added documentation

--- src/main/java/com/example/zooapplicationoop2a3/EnclosureCollection.java
+++ src/main/java/com/example/zooapplicationoop2a3/EnclosureCollection.java
@@ -1,5 +1,13 @@
 package com.example.zooapplicationoop2a3;
 
+/**
+ * has a {@code get()} method.
+ */
 public interface EnclosureCollection {
+    /**
+     * Gets an {@link Object} from a list at a certain index.
+     * @param i index
+     * @return An {@link Object} from a list,
+     */
     Object get(int i);
 }
