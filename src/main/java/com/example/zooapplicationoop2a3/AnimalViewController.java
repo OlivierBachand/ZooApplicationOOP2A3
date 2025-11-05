feeb51e207866292c252a3607381d87751ae8d0c
Max Gosselin <maximgosselin04@gmail.com>
2025-11-05 16:25:28 -0500

    made the AnimalViewController

--- src/main/java/com/example/zooapplicationoop2a3/AnimalViewController.java
+++ src/main/java/com/example/zooapplicationoop2a3/AnimalViewController.java
@@ -1,4 +1,45 @@
 package com.example.zooapplicationoop2a3;
 
-public class AnimalController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class AnimalViewController {
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField ageTextField;
+
+    private Animal currentAnimal;
+
+    public void setAnimal(Animal Animal) {
+        this.currentAnimal = Animal;
+        if(Animal != null)
+        {
+            nameTextField.setText(Animal.getName());
+            ageTextField.setText(String.valueOf(Animal.getAge()));
+        }
+    }
+
+    @FXML
+    protected void onSaveButotnCLick(ActionEvent event) {
+        if (currentAnimal != null) {
+        currentAnimal.setName(nameTextField.getText());
+        try{
+            currentAnimal.setAge(Integer.parseInt(ageTextField.getText()));
+        } catch (NumberFormatException e) {
+            ageTextField.setText("Age is Invalid");
+        }
+        onCancelButtonClick(event);
+        }
+
+    }
+    @FXML
+    protected void onCancelButtonClick(ActionEvent event) {
+        Stage stage = (Stage) nameTextField.getScene().getWindow();
+        stage.close();
+    }
 }
