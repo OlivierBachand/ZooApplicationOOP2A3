395341ef8180d2cd53ea9278111853ca81b7028a
OlivierBachand <olivier.bachand@stu.crc-lennox.qc.ca>
2025-11-04 19:29:35 -0500

    Worked on CompositeEnclosureCollectionController

--- src/main/java/com/example/zooapplicationoop2a3/CompositeEnclosureCollectionController.java
+++ src/main/java/com/example/zooapplicationoop2a3/CompositeEnclosureCollectionController.java
@@ -1,12 +1,15 @@
 package com.example.zooapplicationoop2a3;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -16,34 +19,41 @@ import java.io.IOException;
 public class CompositeEnclosureCollectionController {
     CompositeEnclosureCollection aCEC;
     @FXML
-    public ListView<String> cecListView;
+    public ListView<String> aCECListView;
 
     @FXML
-    public Button openButton;
+    public Button aOpenButton;
 
     public void setCEC(CompositeEnclosureCollection pCEC) {
         this.aCEC = pCEC;
         this.aCEC.addCollection(new CompositeEnclosureCollection("1"));
         this.aCEC.addCollection(new CompositeEnclosureCollection("2"));
         this.aCEC.addCollection(new CompositeEnclosureCollection("3"));
-        this.cecListView.getItems().addAll(aCEC.getItems());
+        this.aCECListView.getItems().addAll(aCEC.getItems());
     }
 
     @FXML
     public void onBackButtonClick() {
-        Stage currentStage = (Stage) this.openButton.getScene().getWindow();
+        Stage currentStage = (Stage) this.aOpenButton.getScene().getWindow();
         currentStage.close();
     }
 
     @FXML
+    public void onCloseButtonClick() {
+        Platform.exit();
+    }
+
+    @FXML
     public void onOpenButtonClick(ActionEvent pEvent) throws IOException {
-        int index = cecListView.getSelectionModel().getSelectedIndex();
-        EnclosureCollection selectedCollection = aCEC.getEnclosureCollection(index);
-        if (index != -1) {
-            if (selectedCollection instanceof CompositeEnclosureCollection) {
-                CompositeEnclosureCollection selectedCEC = (CompositeEnclosureCollection) selectedCollection;
+        try {
+            int index = aCECListView.getSelectionModel().getSelectedIndex();
+            EnclosureCollection selectedCollection = aCEC.getEnclosureCollection(index);
+            if (selectedCollection instanceof CompositeEnclosureCollection selectedCEC) {
                 newCompositeEnclosureCollectionView(pEvent, selectedCEC);
             }
+        }
+        catch (IndexOutOfBoundsException e) {
+            new Alert(Alert.AlertType.ERROR, "Nothing Selected", ButtonType.OK).show();
         }
     }
 
@@ -58,6 +68,7 @@ public class CompositeEnclosureCollectionController {
         nextStage.setTitle(selectedCEC.getName());
         nextStage.initModality(Modality.WINDOW_MODAL);
         nextStage.initOwner(((Node) pEvent.getSource()).getScene().getWindow());
+        nextStage.setResizable(false);
         nextStage.showAndWait();
     }
 }
