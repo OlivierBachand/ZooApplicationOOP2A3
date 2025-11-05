8942eaaa224bf1a41eff040164be2bb03cf608d4
Max Gosselin <maximgosselin04@gmail.com>
2025-11-05 15:46:22 -0500

    removed the get type from the Animal class

--- src/main/java/com/example/zooapplicationoop2a3/Animal.java
+++ src/main/java/com/example/zooapplicationoop2a3/Animal.java
@@ -1,4 +1,29 @@
 package com.example.zooapplicationoop2a3;
 
-public class Animal {
+public abstract class Animal {
+    private String name;
+    private int age;
+
+    public Animal(String name, int age)
+    {
+        this.name =name;
+        this.age = age;
+
+    }
+    // getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+
+        this.age = age;
+    }
+
+
 }
